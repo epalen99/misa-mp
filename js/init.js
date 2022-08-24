3e85513c867f6528242c549974bb1151deb09b59
epalen99 <epalen@ucsd.edu>
2022-08-24 21:34:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/init.js
@@ -0,0 +1,23 @@
+//initializing all needed JS materialize components
+
+//uncomment the code below when needed
+
+
+(function($){
+  $(function(){
+
+    
+    //add the jquery components here (its given on the documentation)
+    //it usually starts with a dollar sign
+ 
+      $('.carousel').carousel();
+      $('.dropdown-trigger').dropdown();
+
+ 
+
+       
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
